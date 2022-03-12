77cfd4041f9f220e77295d5a0523ad231eda3a30
Im-Hassan-wd <alhassansahih@gmail.com>
2022-03-12 21:43:17 +0100

    first ejs file

--- app.js
+++ app.js
@@ -3,13 +3,21 @@ const express = require('express');
 // express app
 const app = express();
 
+// register view engine
+app.set('view engine', 'ejs');
+
 // listen for requests
 app.listen(4002);
 
 app.get('/', (req, res) => {
-    res.send('home');
+    res.send('index');
 });
 
 app.get('/about', (req, res) => {
     res.send('about');
 });
+
+// 404
+app.get('/404', (req, res) => {
+    res.send('404');
+})
