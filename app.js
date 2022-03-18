8d5839aaf563f579e907b1fbd7ed45b72f6b86d5
Im-Hassan-wd <alhassansahih@gmail.com>
2022-03-18 11:20:50 +0100

    security

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
+const uri = require('./dbURI/uri');
 const recipeRoutes = require('./routes/recipeRoutes');
 
 // express app
@@ -15,7 +16,7 @@ app.use(express.urlencoded({ extended: true}));
 app.use(morgan('dev'));
 
 // connect to db
-const dbURI = 'mongodb+srv://weird:test123@learningnode.meubb.mongodb.net/cooking-wick';
+const dbURI = uri;
 mongoose.connect(dbURI)
  .then(result => app.listen(4001))
  .catch(err => console.log(err));
