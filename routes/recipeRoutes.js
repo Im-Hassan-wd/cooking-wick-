7d691ca4af97e3d5471b577264cefc1e93378bda
Im-Hassan-wd <alhassansahih@gmail.com>
2022-03-18 11:46:32 +0100

    controller functions

--- routes/recipeRoutes.js
+++ routes/recipeRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const recipeController = require('../controllers/recipeController');
+const { recipe_get, recipe_create_post, recipe_create_get, recipe_details_get, recipe_delete} = require('../controllers/recipeController');
 const router = express.Router();
 
-router.get('/recipes', recipeController.recipe_get);
-router.post('/recipes', recipeController.recipe_create_post);
-router.get('/recipes/create', recipeController.recipe_create_get);
-router.get('/recipes/:id', recipeController.recipe_details_get);
-router.delete('/recipes/:id', recipeController.recipe_delete);
+router.get('/recipes', recipe_get);
+router.post('/recipes', recipe_create_post);
+router.get('/recipes/create', recipe_create_get);
+router.get('/recipes/:id', recipe_details_get);
+router.delete('/recipes/:id', recipe_delete);
 
 module.exports = router;
